fadedb4f4f0fad082e15d29332c717b46bae260b
Max <maximdyukin@gmail.com>
2021-09-26 15:44:51 +0500

    Initial Commit

---
+++ WorkWithNetwork/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  WorkWithNetwork
+//
+//  Created by Maxim Dukin on 26.09.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
